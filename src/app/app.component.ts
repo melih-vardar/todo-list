df4b7f54414ef8f7ac8749c7a25f752498a8394e
Melih Vardar <mvardar97@gmail.com>
2025-01-20 03:14:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface TodoItem {
+  id:number;
+  task:string;
+  completed:boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+
+  todoList: TodoItem [] = [];
+  newTask:string = '';
+
+  addTask(): void{
+    if(this.newTask.trim() !== ''){
+
+      const newTodoItem : TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+    }
+    this.todoList.push(newTodoItem);
+    this.newTask = '';
+  }
+}
 }
